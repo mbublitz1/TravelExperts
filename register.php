a250eeaab3d9106aa25bc5b627164d8067d7da21
MarsLoucetios <35880688+MarsLoucetios@users.noreply.github.com>
2019-08-07 14:35:56 -0600

    Create register.php

---
+++ register.php
@@ -0,0 +1,13 @@
+<form action="php-bouncer.php" method="post">
+  <p>First Name: <input type="text" name="CustFirstName"></p>
+  <p>Last Name: <input type="text" name="CustLastName"></p>
+  <p>Address: <input type="text" name="CustAddress"></p>
+  <p>City: <input type="text" name="CustCity"></p>
+  <p>Prov: <input type="text" name="CustProv"></p>
+  <p>Postal Code: <input type="text" name="CustPostal"></p>
+  <p>Country: <input type="text" name="CustCountry"></p>
+  <p>Home Phone: <input type="text" name="CustHomePhone"></p>
+  <p>Business Phone: <input type="text" name="CustBusPhone"></p>
+  <p>Email: <input type="text" name="CustEmail"></p>
+  <input type="Submit" name="Submit" value="Submit">
+</form>
