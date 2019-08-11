06c1f9dee866811cfdcb624ba07aeadc46c43d5f
mbublitz <mbublitzdev@gmail.com>
2019-08-10 20:04:15 -0600

    Worked on registration form
    Fixed labels so they center in middle of input boxes.  Removed login menu item by commenting it out.

--- index.php
+++ index.php
@@ -33,32 +33,32 @@
                         <li><a href="#Register">Register</a></li>
                         <li><a href="#AboutUs">About Us</a></li>
                         <li><a href="#ContactUs">Contact Us</a></li>
-                        <li>
-                            <div class="dropdown">
-                                <button class="loginlnk dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Login
-                                </button>
-                                <form class="dropdown-menu p-4">
-                                    <div class="form-group">
-                                        <label for="exampleDropdownFormEmail2">Email address</label>
-                                        <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="exampleDropdownFormPassword2">Password</label>
-                                        <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="form-check">
-                                            <input type="checkbox" class="form-check-input" id="dropdownCheck2">
-                                            <label class="form-check-label" for="dropdownCheck2">
-                                                Remember me
-                                            </label>
-                                        </div>
-                                    </div>
-                                    <button type="submit" class="btn btn-primary">Sign in</button>
-                                </form>
-                            </div>
-                        </li>
+<!--                        <li>-->
+<!--                            <div class="dropdown">-->
+<!--                                <button class="loginlnk dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
+<!--                                    Login-->
+<!--                                </button>-->
+<!--                                <form class="dropdown-menu p-4">-->
+<!--                                    <div class="form-group">-->
+<!--                                        <label for="exampleDropdownFormEmail2">Email address</label>-->
+<!--                                        <input type="email" class="form-control" id="exampleDropdownFormEmail2" placeholder="email@example.com">-->
+<!--                                    </div>-->
+<!--                                    <div class="form-group">-->
+<!--                                        <label for="exampleDropdownFormPassword2">Password</label>-->
+<!--                                        <input type="password" class="form-control" id="exampleDropdownFormPassword2" placeholder="Password">-->
+<!--                                    </div>-->
+<!--                                    <div class="form-group">-->
+<!--                                        <div class="form-check">-->
+<!--                                            <input type="checkbox" class="form-check-input" id="dropdownCheck2">-->
+<!--                                            <label class="form-check-label" for="dropdownCheck2">-->
+<!--                                                Remember me-->
+<!--                                            </label>-->
+<!--                                        </div>-->
+<!--                                    </div>-->
+<!--                                    <button type="submit" class="btn btn-primary">Sign in</button>-->
+<!--                                </form>-->
+<!--                            </div>-->
+<!--                        </li>-->
 
                     </ul>
                 </div>
--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <form action="php-bouncer.php" method="post">
-    <div id="form" class="pgrow">
+    <div id="form" class="pgrow signup-form">
         <div class="col span-1-of-3">
             <p>Please register with our system in order to start booking trips.</p>
 
@@ -17,56 +17,30 @@
             ?>
 
         </div>
-        <div class="col span-1-of-3">
-            <ul class="form--inputs">
-                <li>
-                    <label for="CustFirstName">First Name:</label>
-                    <input type="text" name="CustFirstName">
-                </li>
-                <li>
-                    <label for="CustLastName">Last Name:</label>
-                    <input type="text" name="CustLastName">
-                </li>
-                <li>
-                    <label for="CustAddress">Address:</label>
-                    <input type="text" name="CustAddress">
-                </li>
-                <li>
-                    <label for="CustCity">City:</label>
-                    <input type="text" name="CustCity">
-                </li>
-                <li>
-                    <label for="CustProv">Prov:</label>
-                    <input type="text" name="CustProv">
-                </li>
-                <li>
-                    <label for="CustPostal">Postal Code:</label>
-                    <input type="text" name="CustPostal">
-                </li>
-                <li>
-                    <label for="CustCountry">Country:</label>
-                    <input type="text" name="CustCountry">
-                </li>
-            </ul>
+        <div class="col span-1-of-3 signup-page">
+            <label for="CustFirstName">First Name:</label>
+            <input type="text" name="CustFirstName">
+            <label for="CustLastName">Last Name:</label>
+            <input type="text" name="CustLastName">
+            <label for="CustAddress">Address:</label>
+            <input type="text" name="CustAddress">
+            <label for="CustCity">City:</label>
+            <input type="text" name="CustCity">
+            <label for="CustProv">Prov:</label>
+            <input type="text" name="CustProv">
+            <label for="CustPostal">Postal Code:</label>
+            <input type="text" name="CustPostal">
+            <label for="CustCountry">Country:</label>
+            <input type="text" name="CustCountry">
         </div>
         <div class="col span-1-of-3">
-            <ul class="form--inputs">
-                <li>
-                    <label for="CustHomePhone">Home No:</label>
-                    <input type="text" name="CustHomePhone">
-                </li>
-                <li>
-                    <label for="CustBusPhone">Business No:</label>
-                    <input type="text" name="CustBusPhone">
-                </li>
-                <li>
-                    <label for="CustEmail">Email:</label>
-                    <input type="text" name="CustEmail">
-                </li>
-                <li>
-                    <input type="Submit" name="Submit" value="Submit">
-                </li>
-            </ul>
+            <label for="CustHomePhone">Home No:</label>
+            <input type="text" name="CustHomePhone">
+            <label for="CustBusPhone">Business No:</label>
+            <input type="text" name="CustBusPhone">
+            <label for="CustEmail">Email:</label>
+            <input type="text" name="CustEmail">
+            <input type="Submit" name="Submit" value="Submit">
         </div>
     </div>
 </form>
