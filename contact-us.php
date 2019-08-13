6a93e10ef48d2670ad81cd14995318d0cb2569ea
MarsLoucetios <35880688+MarsLoucetios@users.noreply.github.com>
2019-08-13 11:01:51 -0600

    Update contact-us.php

--- contact-us.php
+++ contact-us.php
@@ -1,11 +1,13 @@
 <!--            Contact Section - Sumit & John-->
 
 <h3>Get In Touch</h3>
-<h4>Calgary</h4>
 <div class="contact pgrow">
     <div class="gridcol span-1-of-3">
         <em class="fas fa-phone-square-alt fa-3x"></em><br><br>
-        <p><strong>PHONE:</strong></p><br>
+        <p><strong>PHONE:</strong></p>
+        <br>
+        <p><strong>Calgary</strong></p>
+        <br>
 
         <?php
         $query = "SELECT AgncyCity, AgncyPhone, AgncyFax FROM agencies
@@ -23,86 +25,58 @@
         }
           ?>
 
-    </div>
-    <div class="gridcol span-1-of-3">
-        <em class="fas fa-envelope-open-text fa-3x"></em><br><br>
-        <p><strong>EMAIL:</strong></p>
-        <br>
+          <p><strong>Okotoks</strong></p>
+          <br>
 
-        <?php
-          $query = "SELECT * FROM agents
-          INNER JOIN agencies ON agencies.AgencyId = agents.AgencyId
-          WHERE AgncyCity = 'Calgary'
-          ORDER BY agencies.AgncyCity, agents.AgtFirstName";
-          $res = $conn->query($query);
-
-          while ($row = $res->fetch_assoc()) {
-            echo $row['AgtFirstName'] . " " . $row['AgtLastName'];
-            echo "<br>";
-            echo $row['AgtPosition'];
-            echo "<br>";
-            echo "<a href=\"mailto:" . $row['AgtEmail'] . "\">" . $row['AgtEmail'] . "</a>";
-            echo "<br><br><br>";
-          }
-        ?>
-
-    </div>
-
-    <div class="gridcol span-1-of-3">
-        <em class="fas fa-map-marker-alt fa-3x"></em><br><br>
-        <p><strong>ADDRESS:</strong></p><br>
-        <?php
-        $query = "SELECT AgncyAddress, AgncyCity, AgncyProv, AgncyCountry, AgncyPostal  FROM agencies
-        WHERE AgncyCity = 'Calgary'
-        ORDER BY AgncyCity";
-        $res = $conn->query($query);
-
-        while ($row = $res->fetch_assoc()) {
-          echo $row['AgncyAddress'];
-          echo "<br>";
-          echo $row['AgncyCity'] . ", " . $row['AgncyProv'] . ", " . $row['AgncyCountry'];
-          echo "<br>";
-          echo $row['AgncyPostal'];
-          echo "<br><br><br>";
-        }
-          ?>
-    </div>
-</div>
-
-
-
-<h4>Okotoks</h4>
-<div class="contact pgrow">
-    <div class="gridcol span-1-of-3">
-        <em class="fas fa-phone-square-alt fa-3x"></em><br><br>
-        <p><strong>PHONE:</strong></p><br>
-
-        <?php
-        $query = "SELECT AgncyCity, AgncyPhone, AgncyFax FROM agencies
-        WHERE AgncyCity = 'Okotoks'
-        ORDER BY AgncyCity";
-        $res = $conn->query($query);
-
-        while ($row = $res->fetch_assoc()) {
-          echo "<p>Phone:</p>";
-          echo $row['AgncyPhone'];
-          echo "<br><br>";
-          echo "<p>Fax:</p>";
-          echo $row['AgncyFax'];
-          echo "<br><br><br>";
-        }
-          ?>
-
-    </div>
-    <div class="gridcol span-1-of-3">
-        <em class="fas fa-envelope-open-text fa-3x"></em><br><br>
-        <p><strong>EMAIL:</strong></p>
-        <br>
-
-        <?php
-          $query = "SELECT * FROM agents
-          INNER JOIN agencies ON agencies.AgencyId = agents.AgencyId
+          <?php
+          $query = "SELECT AgncyCity, AgncyPhone, AgncyFax FROM agencies
           WHERE AgncyCity = 'Okotoks'
+          ORDER BY AgncyCity";
+          $res = $conn->query($query);
+
+          while ($row = $res->fetch_assoc()) {
+            echo "<p>Phone:</p>";
+            echo $row['AgncyPhone'];
+            echo "<br><br>";
+            echo "<p>Fax:</p>";
+            echo $row['AgncyFax'];
+            echo "<br><br><br>";
+          }
+            ?>
+
+
+    </div>
+    <div class="gridcol span-1-of-3">
+        <em class="fas fa-envelope-open-text fa-3x"></em><br><br>
+        <p><strong>EMAIL:</strong></p>
+        <br>
+        <p><strong>Calgary</strong></p>
+        <br>
+
+        <?php
+          $query = "SELECT * FROM agents
+          INNER JOIN agencies ON agencies.AgencyId = agents.AgencyId
+          WHERE AgncyCity = 'Calgary' AND AgtPosition = 'Senior Agent'
+          ORDER BY agencies.AgncyCity, agents.AgtFirstName";
+          $res = $conn->query($query);
+
+          while ($row = $res->fetch_assoc()) {
+            echo $row['AgtFirstName'] . " " . $row['AgtLastName'];
+            echo "<br>";
+            echo $row['AgtPosition'];
+            echo "<br>";
+            echo "<a href=\"mailto:" . $row['AgtEmail'] . "\">" . $row['AgtEmail'] . "</a>";
+            echo "<br><br><br>";
+          }
+        ?>
+
+        <p><strong>Okotoks</strong></p>
+        <br>
+
+        <?php
+          $query = "SELECT * FROM agents
+          INNER JOIN agencies ON agencies.AgencyId = agents.AgencyId
+          WHERE AgncyCity = 'Okotoks' AND AgtPosition = 'Senior Agent'
           ORDER BY agencies.AgncyCity, agents.AgtFirstName";
           $res = $conn->query($query);
 
@@ -120,10 +94,14 @@
 
     <div class="gridcol span-1-of-3">
         <em class="fas fa-map-marker-alt fa-3x"></em><br><br>
-        <p><strong>ADDRESS:</strong></p><br>
+        <p><strong>ADDRESS:</strong></p>
+        <br>
+        <p><strong>Okotoks</strong></p>
+        <br>
+
         <?php
         $query = "SELECT AgncyAddress, AgncyCity, AgncyProv, AgncyCountry, AgncyPostal  FROM agencies
-        WHERE AgncyCity = 'Okotoks'
+        WHERE AgncyCity = 'Calgary'
         ORDER BY AgncyCity";
         $res = $conn->query($query);
 
@@ -136,5 +114,25 @@
           echo "<br><br><br>";
         }
           ?>
+
+          <p><strong>Okotoks</strong></p>
+          <br>
+
+          <?php
+          $query = "SELECT AgncyAddress, AgncyCity, AgncyProv, AgncyCountry, AgncyPostal  FROM agencies
+          WHERE AgncyCity = 'Okotoks'
+          ORDER BY AgncyCity";
+          $res = $conn->query($query);
+
+          while ($row = $res->fetch_assoc()) {
+            echo $row['AgncyAddress'];
+            echo "<br>";
+            echo $row['AgncyCity'] . ", " . $row['AgncyProv'] . ", " . $row['AgncyCountry'];
+            echo "<br>";
+            echo $row['AgncyPostal'];
+            echo "<br><br><br>";
+          }
+            ?>
+
     </div>
 </div>
